81796a6d41c399844748b29dfb985991fa649370
Mahdi Hasani <mahdi.hasani@chargoon.net>
2019-09-23 15:17:06 +0330

    add js to blazor

--- wwwroot/docs/js/script.js
+++ wwwroot/docs/js/script.js
@@ -1,44 +1,39 @@
+function  sidebarClick() {
+  var sidebar = $('body').hasClass('hide-sidebar');
+  var pagesmall = $('body').hasClass('page-small');
 
-$(window).resize(function () {
-  exampleJsFunctions.resizePage();
-});
-
-exampleJsFunctions.resizePage()
-
-
-
-window.exampleJsFunctions = {
-  sidebarClick: function() {
-    var sidebar = $('body').hasClass('hide-sidebar');
-    var pagesmall = $('body').hasClass('page-small');
-  
-    if (pagesmall) {
+  if (pagesmall) {
       $('body').removeClass('hide-sidebar');
       $('body').removeClass('page-small');
-    }
-    else {
+  }
+  else {
       if (sidebar) {
-        $('body').removeClass('hide-sidebar');
-        $('body').removeClass('page-small');
+          $('body').removeClass('hide-sidebar');
+          $('body').removeClass('page-small');
       }
       else {
-        $('body').addClass('hide-sidebar');
-        $('body').removeClass('page-small');
+          $('body').addClass('hide-sidebar');
+          $('body').removeClass('page-small');
       }
-    }
-    console.log(sidebar)
-  },
-  resizePage :function() {
-    var width = $(window).width();
-    console.log(width);
-  
-    if (width < 767) {
+  }
+  console.log(sidebar)
+}
+function resizePage () {
+  var width = $(window).width();
+  console.log(width);
+
+  if (width < 767) {
       $('body').addClass('page-small');
       $('body').removeClass('hide-sidebar');
-    }
-    else {
+  }
+  else {
       $('body').removeClass('page-small');
       $('body').removeClass('hide-sidebar');
-    }
   }
-};
+}
+
+resizePage();
+
+$(window).resize(function () {
+  resizePage();
+});
